9c9a240f311bae12a52354fb4af66647ed12d3b7
forkanbin14 <99709774+forkanbin14@users.noreply.github.com>
2022-11-07 14:11:53 +0600

    New py

---
+++ New.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python2
+# -*- coding: utf-8
+
+#AUTHOR : Forkan 
+#Welcome Bruh👽👽
+
+
+try:
+	import os,sys,time,platform,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib,requests,uuid,string,subprocess
+	from multiprocessing.pool import ThreadPool
+	from requests.exceptions import ConnectionError
+except ImportError:
+	os.system("pip2 install requests lolcat")
+	os.system("python New.py")
