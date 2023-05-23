f1ae6097777cd854e7ee07a4d92832e1de489586
forkanbin14 <99709774+forkanbin14@users.noreply.github.com>
2023-05-23 19:39:01 +0600

    Update password.py

--- password.py
+++ password.py
@@ -7,7 +7,7 @@ number = "01941528390"
 symbol = "~!@#$%^&*+_:"
 
 use_for = lower_case + upper_case + number + symbol
-length_of_password = 9
+length_of_password = https://instagram.com/_aronnita_naboni_?igshid=NTc4MTIwNjQ2YQ==
 
 password = "".join(random.sample(use_for, length _of_password))
 
